9372e6e9ffe3c342409f9e051c447d8f3e6f2829
Alexandrmrk <alexandrmrk@mail.ru>
2020-05-27 22:29:25 +0300

    В проект добавлена jwt авторизация и её проверка

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -2,6 +2,7 @@ import User from '../models/User';
 import TryCatch from '../decorators/TryCatchMiddlewareDecorator';
 import HttpError from '../exeptions/HttpError';
 import { hashPassword, checkPassword } from '../helpers/password';
+import { createAuthToken } from '../helpers/auth';
 
 class AuthController {
   @TryCatch
@@ -9,11 +10,15 @@ class AuthController {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
-    if (!user || !await checkPassword(password, user.password)) {
+    if (!user || !(await checkPassword(password, user.password))) {
       throw new HttpError('Incorrect login or password', 401);
     }
 
-    res.json({ status: true, user });
+    /* eslint no-underscore-dangle: ["error", {"allow": ["_id"]}] */
+    const authToken = await createAuthToken({ id: user._id.toString() });
+    delete user.password;
+
+    res.json({ status: true, user, authToken });
   }
 
   @TryCatch
